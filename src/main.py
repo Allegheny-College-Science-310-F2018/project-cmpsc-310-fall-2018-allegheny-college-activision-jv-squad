3d026f4cbdee517bec131bebfcc9407a3a2c695b
Austin Bristol <bristola@aldenv175.allegheny.edu>
2018-11-26 16:01:46 -0500

    Some kinda not working path finding

--- src/main.py
+++ src/main.py
@@ -6,9 +6,10 @@ import pymunk.pygame_util
 from pymunk import Vec2d
 import math, sys, random
 import time
+from fractions import Fraction
 
 # TODO: In order to get the AI to go to the ball, we will need to take the two
-# locations and make some linear linear equation for it using the line formula.
+# locations and make some linear equation for it using the line formula.
 # Then, we use the slope of this line in order to get some rise over run
 # fraction. We the use this to develop a sequence of inputs to be passed through
 # the movement function so that the AI moves to the ball.
@@ -75,6 +76,52 @@ def createBall(space, friction, mass, radius, x, y, color):
     space.add(body, shape)
     return body, shape
 
+def path_find(ai_player, ai_goal_location_x, ai_goal_location_y):
+    ai_current_path_y = ai_goal_location_y - ai_player.position.y # Up / Down
+    ai_current_path_x = ai_goal_location_x - ai_player.position.x # Left / Right
+    slope = ai_current_path_y / ai_current_path_x
+    slope = round(slope, 1)
+    rise_run = Fraction(str(slope))
+    rise = rise_run.numerator
+    run = rise_run.denominator
+    keys_to_press = list()
+
+    print("Rise = "+str(rise))
+    print("Run = "+str(run))
+
+    for r1, r2 in zip(range(0,abs(rise)), range(0,abs(run))):
+        keys = [0 for i in range(0,323)]
+        if rise >= 0:
+            keys[K_DOWN] = 1
+        else:
+            keys[K_UP] = 1
+        if run >= 0:
+            keys[K_LEFT] = 1
+        else:
+            keys[K_RIGHT] = 1
+        keys_to_press.append(tuple(keys))
+
+    for r in range(0, abs(abs(rise)-abs(run))):
+        keys = [0 for i in range(0,323)]
+        if (rise > run):
+            if rise >= 0:
+                keys[K_DOWN] = 1
+            else:
+                keys[K_UP] = 1
+        else:
+            if run >= 0:
+                keys[K_LEFT] = 1
+            else:
+                keys[K_RIGHT] = 1
+        keys_to_press.append(tuple(keys))
+
+    if len(keys_to_press % 2 == 1):
+        del keys_to_press[len(keys_to_press) - 1]
+
+    # print(str(keys_to_press))
+
+    return keys_to_press
+
 def get_distance(obj1, obj2):
     x1 = obj1.position.x
     y1 = obj1.position.y
@@ -84,9 +131,10 @@ def get_distance(obj1, obj2):
     distance = math.sqrt(distance)
     return distance
 
-def make_impulse(player, ball, magnitude):
+def make_impulse(player, ball, magnitude, speeds):
+    magnitude1 = magnitude + (max(speeds)/ (len(speeds)*2))
     if (get_distance(player, ball) < (list(player.shapes)[0].radius + list(ball.shapes)[0].radius)):
-        impulse = Vec2d(-1*(player.position.x-ball.position.x)*(magnitude), -1*(player.position.y-ball.position.y)*(magnitude))
+        impulse = Vec2d(-1*(player.position.x-ball.position.x)*(magnitude1), -1*(player.position.y-ball.position.y)*(magnitude1))
         ball.apply_impulse_at_world_point((impulse), (player.position))
 
 def move_player(player, speeds, keys, top_speed, acceleration):
@@ -140,18 +188,36 @@ def move_player(player, speeds, keys, top_speed, acceleration):
         player.position += Vec2d(1,0) * speeds[3]
     return speeds
 
+def print_direction(keys):
+    dir = ""
+    if keys[K_UP]:
+        dir = dir + "UP "
+    if keys[K_DOWN]:
+        dir = dir + "DOWN "
+    if keys[K_LEFT]:
+        dir = dir + "LEFT "
+    if keys[K_RIGHT]:
+        dir = dir + "RIGHT "
+    print(dir)
+
 def run():
     screen, space, draw_options, clock, myfont = setup()
     border = createBorder(space)
     space.add(border)
     running = True
-    ball, ball_shape = createBall(space, .1, 3, 10, 350, 200, "white")
+    ball, ball_shape = createBall(space, .1, 3, 10, 350, 300, "white")
     player, player_shape = createBall(space, 1.0, 1000000, 13, 100, 200, "dodgerblue4")
+    ai_player, ai_player_shape = createBall(space, 1.0, 1000000, 13, 600, 200, "red3")
+
+    ai_keys_to_press = []
+    ai_goal_location_x = None
+    ai_goal_location_y = None
 
     team1Score = 0
     team2Score = 0
 
     speeds = [0,0,0,0]
+    speeds_ai = [0,0,0,0]
     acceleration = .1
     top_speed = 3.0
 
@@ -205,11 +271,27 @@ def run():
             time.sleep(5)
             break
 
+
+        if (ai_goal_location_x is None or ai_goal_location_x != ball.position.x or ai_goal_location_y is None or ai_goal_location_y != ball.position.y):
+            ai_goal_location_x = ball.position.x
+            ai_goal_location_y = ball.position.y
+            ai_keys_to_press = path_find(ai_player, ai_goal_location_x, ai_goal_location_y)
+            for a in ai_keys_to_press:
+                print_direction(a)
+
+        if (len(ai_keys_to_press) > 0):
+            # print_direction(ai_keys_to_press[0])
+            speeds_ai = move_player(ai_player, speeds_ai, ai_keys_to_press[0], top_speed, acceleration)
+            ai_keys_to_press.append(ai_keys_to_press[0])
+            del ai_keys_to_press[0]
+        else:
+            speeds_ai = move_player(ai_player, speeds_ai, tuple([0 for i in range(0,323)]), top_speed, acceleration)
+
         keys = pygame.key.get_pressed()
 
         speeds = move_player(player, speeds, keys, top_speed, acceleration)
 
-        make_impulse(player, ball, (1/5))
+        make_impulse(player, ball, (1/5), speeds)
 
         screen.fill(pygame.Color(21, 155, 50, 1))
         draw_lines(screen)
